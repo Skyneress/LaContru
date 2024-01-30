c95cbc07dca240182e367181308d06c08b941e18
Skyneress <el.montesinos@duocuc.cl>
2024-01-30 16:48:29 -0300

    Primer avance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'Inicio',
+        title: 'Pagina principal',
+        component: InicioComponent
+    },
+    {
+        path: '',
+        redirectTo: '/Inicio',
+        pathMatch: 'full'
+
+    }
+];
